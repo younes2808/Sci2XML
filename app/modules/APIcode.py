67693f448f53e4839ed8b3f1876c3a2e95d50456
SameNilsen <mort1.nilsen@gmail.com>
2025-03-13 12:33:20 +0100

    Fix of tableparser xml-variables

--- app/modules/APIcode.py
+++ app/modules/APIcode.py
@@ -409,12 +409,12 @@ def API():
       print("-- Table Parser --")
       ### Run the xml and pdf through the tableparser before processing further. Could also be done after the processing of the other elements instead.
       # Ready the files
-      files = {"grobid_xml": ("xmlfile.xml", xml_input, "application/json"), "pdf": ("pdffile.pdf", byte_data_PDF)}
+      files = {"grobid_xml": ("xmlfile.xml", string_data_XML, "application/json"), "pdf": ("pdffile.pdf", byte_data_PDF)}
 
       # Send to API endpoint for processing of tables
       response = requests.post("http://172.28.0.12:8000/parseTable", files=files)
       print("response", response)
-      xml_input = response.text
+      string_data_XML = response.text
 
       ##  Starting classifier ##
       print("-- Starting Classifier --")
